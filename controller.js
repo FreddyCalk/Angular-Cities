94547dc484f5fec595ddca805867de539d13b423
Freddy Calk <freddycalk@gmail.com>
2015-12-04 13:11:05 -0500

    angular cities

---
+++ controller.js
@@ -0,0 +1,12 @@
+// Angular App Module and controller
+angular.module('myApp',[]).controller('mapController',function($scope){
+
+	var mapOption = {
+		zoom: 4,
+		center: new google.maps.LatLng(40.000,-98.000),
+		mapTypeId: google.maps.mapTypeId.TERRAIN
+	}
+
+
+
+})
